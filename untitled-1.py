5db455bf43a90749debb6b27c626a8a266bdb69b
WetOnionBoi <37384973+WetOnionBoi@users.noreply.github.com>
2019-02-13 15:03:02 +1300

    Update untitled-1.py

--- untitled-1.py
+++ untitled-1.py
@@ -1,4 +1,12 @@
                #Ver 1.1 Created test data for ticket variable
+  
+  
+  
+  
+  
+  
+  
+  #Test Data
 tickets = [
 Ticket("Captain America", "America@gmail.com", "02.11.2015", False),
           Ticket("John Wick", "Yespapa@gmail.com", "01.01.2001", False),
