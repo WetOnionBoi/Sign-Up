920c8ce3c245233d3c328c142663123c0f7f8c19
WetOnionBoi <37384973+WetOnionBoi@users.noreply.github.com>
2019-02-13 15:02:08 +1300

    Add files via upload

---
+++ untitled-1.py
@@ -0,0 +1,7 @@
+               #Ver 1.1 Created test data for ticket variable
+tickets = [
+Ticket("Captain America", "America@gmail.com", "02.11.2015", False),
+          Ticket("John Wick", "Yespapa@gmail.com", "01.01.2001", False),
+          Ticket("Bat Man", "Battery@gmail.com", "30.07.1990", False),
+          Ticket("Spider Man", "Spidey@gmail.com", "14.02.1920", False)
+          ]
